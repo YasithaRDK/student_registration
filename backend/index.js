4fe7cd08d528038292d346ad93c29d31c59c474f
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2023-12-16 23:48:25 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use("/api/allocate-subjects", allocateSubjectRouter);
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
     res.sendFile(
